e45e95a255526c332037e32e7250367a97e9750a
root <root@Phenomenal.localdomain>
2018-09-13 16:54:54 +0500

    first commit contains error

---
+++ feature/src/main/java/com/personnel/alibaba/walksafe/feature/MainActivity.java
@@ -0,0 +1,47 @@
+package com.personnel.alibaba.walksafe.feature;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    double ax, ay, az; // three directions only
+    TextView x =  findViewById(R.id.x_axis);
+    TextView y =  findViewById(R.id.y_axis);
+    TextView z =  findViewById(R.id.z_axis);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            ax = event.values[0];
+            ay = event.values[1];
+            az = event.values[2];
+            x.setText(Double.toString(ax));
+            y.setText(Double.toString(ay));
+            z.setText(Double.toString(az));
+        }
+    }
+
+}
